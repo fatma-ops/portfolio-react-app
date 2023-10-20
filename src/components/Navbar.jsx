f02e24d9c6aff9ba0eaa734753047f3591942c4d
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-10-21 00:23:01 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import logo from '../assetes/logo1.png'
+import{FaBars,FaTimes} from 'react-icons/fa'
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      
+      <div>
+        <img src={logo} alt="Logo Image" style={{width:'50px'}} />
+      </div>
+
+      {/*menu */}
+      <div className='hidden md:flex' >
+        <ul className='flex'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+      </div>
+     {/* hamburger*/}
+     <div className='hidden'>
+     <FaBars/>
+     </div>
+
+     {/* mobile menu*/}
+     <div className='hidden'>
+        <ul>
+            <li> home</li>
+            <li> about</li>
+            <li> home</li>
+            <li> home</li>
+        </ul>
+     </div>
+
+     {/* social icons*/}
+     <div className='hidden'>
+        
+     </div>
+
+
     </div>
   )
 }
