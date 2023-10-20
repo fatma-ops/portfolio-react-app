11aff1d76da1f0ab64a36daa0a4cf4a0513d57c3
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-10-20 23:08:51 +0100

    test

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      
+    </div>
+  )
+}
+
+export default Navbar
+
